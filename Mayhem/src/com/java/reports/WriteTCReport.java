96cbd1dc386996139a20d0a2d7bb8da192ffe8c8
Pratik <pratikbodkhe@gmail.com>
2015-01-14 23:14:09 +0530

    Created report structure

--- Mayhem/src/com/java/reports/WriteTCReport.java
+++ Mayhem/src/com/java/reports/WriteTCReport.java
@@ -7,99 +7,58 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import com.java.commons.*;
+
+import org.apache.commons.io.FileUtils;
 
 import com.java.commons.Constants;
 
 public class WriteTCReport {
-	
-	public WriteTCReport(){
+
+	public WriteTCReport() {
 		String reportFolderName = new SimpleDateFormat(" d MMM yyyy")
-		.format(new Date());
-		File folder = new File(Constants.REPORT_PATH+reportFolderName);
+				.format(new Date());
+		File folder = new File(Constants.REPORT_PATH + reportFolderName);
 		if (!folder.exists()) {
 			folder.mkdirs();
 		}
-		
-		
+
 	}
 
 	public void testCaseExecutionStatus(String tc_ID) {
-		
+
 		String filename = new SimpleDateFormat(" d MMM yyyy HH_mm_ss")
-		.format(new Date());
-		File file = new File(Constants.REPORT_PATH+filename+".html");
+				.format(new Date());
+		File file = new File(Constants.REPORT_PATH + filename + ".html");
 		try {
+			File htmlTemplateFile = new File(Constants.TEMPLATE_PATH
+					+ "template.html");
+			String htmlString = FileUtils.readFileToString(htmlTemplateFile);
+			String title = "Execution Report "
+					+ new SimpleDateFormat(" dd MMM yyyy").format(new Date());
+			Variables variables = new Variables();
+			String result = " <tr> <td>" + tc_ID + "</td> <td>"
+					+ variables.getTC_Name() + "</td> <td>"
+					+ variables.getStartedOn() + "</td> <td>"
+					+ variables.getCompletedOn() + "</td> <td>1 min</td> <td>"
+					+ System.getProperty("user.name") + "</td> </tr>";
+			htmlString = htmlString.replace("$title", title);
+			int index = htmlString.lastIndexOf("$result");
+			System.out.println("index     - - - - "+index);
+			htmlString = htmlString.substring(0,
+					htmlString.lastIndexOf("$result"))
+					+ result
+					+ htmlString.substring(htmlString.lastIndexOf("$result"));
 			file.createNewFile();
-			//FileInputStream fin = new FileInputStream(file);
-			String content = "<!DOCTYPE html><html><body><table><tr><th>Firstname</th><th>Lastname</th>		<th>"+tc_ID+"</th></tr></body></html>";
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(content);
-			bw.close();
- 
-			System.out.println("Done");
-			
-			
-			
-			
-			
+			FileUtils.writeStringToFile(file, htmlString);
+
+			System.out.println(" Report Writing Done");
+
 		} catch (IOException e) {
 			System.out.println("Write Failed");
 			e.printStackTrace();
 		}
-		
-		
-		/* <!DOCTYPE html>
-<html>
 
-<head>
-<style>
-table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-    text-align: center
-}
-th, td {
-    padding: 5px;
-}
-th {
-    text-align: center;
-}
-</style>
-</head>
-
-<body>
-
-<table style="width:100%">
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>		
-    <th>Points</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>		
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>		
-    <td>94</td>
-  </tr>
-  <tr>
-    <td>John</td>
-    <td>Doe</td>		
-    <td>80</td>
-  </tr>
-</table>
-
-</body>
-</html>
-*/
-		
 	}
-
-	
-	
 
 }
