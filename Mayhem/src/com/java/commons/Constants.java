6c912af4f5633f3d60e550b3dbf8e7525bdb46c1
Pratik <pratikbodkhe@gmail.com>
2015-01-14 23:13:31 +0530

    Added report html template path

--- Mayhem/src/com/java/commons/Constants.java
+++ Mayhem/src/com/java/commons/Constants.java
@@ -3,7 +3,8 @@ package com.java.commons;
 public class Constants {
 	
 	public static final String URLPath = "http://www.google.co.uk";
-	public static final String chromeEXE = "C://PB//adt-bundle-windows-x86-20140321//Required jars and files//chromedriver.exe";
+	public static final String chromeEXE = System.getProperty("user.dir")+"\\Resources\\chromedriver.exe";
 	public static final String EXCEL_PATH = System.getProperty("user.dir")+"\\Resources\\Excel\\";
 	public static final String REPORT_PATH = System.getProperty("user.dir")+"\\Reports\\";
+	public static final String TEMPLATE_PATH = System.getProperty("user.dir")+"\\Resources\\Report Template\\";
 }
